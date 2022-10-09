ff4be30ccdb8d8c31222af01bb4bbeb5296a32ca
Adam Petroff <adampetroff11235@gmail.com>
2022-10-09 16:08:11 +0200

    try catch

--- src/utils/groupTransactionsByDate.ts
+++ src/utils/groupTransactionsByDate.ts
@@ -1,18 +1,20 @@
-import { ITransaction } from 'types';
+import { ITransaction } from "types";
 
 export const groupTransactionsByDate = (transactions: Array<ITransaction>) => {
-	const groupedTransactions: {
-		[key: string]: Array<ITransaction>;
-	} = {};
+  const groupedTransactions: {
+    [key: string]: Array<ITransaction>;
+  } = {};
 
-	transactions.forEach((txn) => {
-		const [date] = txn.date.toISOString().split('T');
-		if (date in groupedTransactions) {
-			groupedTransactions[date].push(txn);
-		} else {
-			groupedTransactions[date] = [txn];
-		}
-	});
+  transactions.forEach((txn) => {
+    try {
+      const [date] = txn.date.toISOString().split("T");
+      if (date in groupedTransactions) {
+        groupedTransactions[date].push(txn);
+      } else {
+        groupedTransactions[date] = [txn];
+      }
+    } catch {}
+  });
 
-	return groupedTransactions;
+  return groupedTransactions;
 };
